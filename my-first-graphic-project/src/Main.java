7e1ad9a80e79296dcc49cf7e089436939b7ba926
Camila <camila.lpessoa@gmail.com>
2024-10-08 17:56:36 +0100

    commited

---
+++ my-first-graphic-project/src/Main.java
@@ -0,0 +1,23 @@
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Ellipse;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(10, 10, 1000, 1000);
+        rectangle.draw();
+        rectangle.setColor(Color.MAGENTA);
+        rectangle.fill();
+
+        Ellipse ellipse = new Ellipse(400, 400, 250, 250);
+        ellipse.draw();
+        ellipse.setColor(Color.GREEN);
+        ellipse.fill();
+
+        Text text = new Text(480,500, "the green sun");
+        text.draw();
+        text.setColor(Color.BLACK);
+    }
+}
